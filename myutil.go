c390fbcfae38ac01004e877a8b4fca332e1642ad
Mark Sauter <dr.funk.mark@gmail.com>
2019-02-04 13:45:30 -0600

    change functionality of Trace, rm message arg add skip arg

--- myutil.go
+++ myutil.go
@@ -8,17 +8,17 @@ import (
 	"runtime"
 )
 
-// Returns string with function name, line number, and message for logging
-// purposes.
-func Trace(message string) string {
+// Returns string with function name and line number. The argument skip is the
+// number of stack frames to skip, with 0 identifying the frame from which Trace
+// was called, 1 identifying the frame above, etc.
+func Trace(skip int) string {
 	pc := make([]uintptr, 15)
-	n := runtime.Callers(2, pc)
+	n := runtime.Callers(2+skip, pc)
 	frames := runtime.CallersFrames(pc[:n])
 	frame, _ := frames.Next()
 	return fmt.Sprintf(
-		"%s:%d: %s",
+		"%s:%d:",
 		filepath.Base(frame.Function),
 		frame.Line,
-		message,
 	)
 }
